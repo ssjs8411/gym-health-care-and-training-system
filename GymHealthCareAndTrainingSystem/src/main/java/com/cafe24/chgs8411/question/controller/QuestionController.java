f1bfafba740ce7c6b4efd93bdc9f33b333592a68
Administrator <Administrator@smart20116>
2018-09-28 17:04:19 +0900

    [김소희] QuestionController 추가

--- GymHealthCareAndTrainingSystem/src/main/java/com/cafe24/chgs8411/question/controller/QuestionController.java
+++ GymHealthCareAndTrainingSystem/src/main/java/com/cafe24/chgs8411/question/controller/QuestionController.java
@@ -1,8 +1,11 @@
 /* 김소희 2018-09-27 / QuestionController */
 package com.cafe24.chgs8411.question.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,6 +18,13 @@ public class QuestionController {
 	@Autowired
 	private QuestionService questionService;
 	
+	@RequestMapping (value="/questionList", method = RequestMethod.GET)
+	public String questionList(Model model) {
+		List<Question> list = questionService.questionList();
+		model.addAttribute("lsit", list);
+		return "question/questionList";
+		
+	}
 	@RequestMapping (value="/questionInsert", method = RequestMethod.POST)
 	public String questionInsert(Question question) {
 		System.out.println("questionInsertAction");
@@ -26,7 +36,7 @@ public class QuestionController {
 	@RequestMapping (value="/questionInserst", method = RequestMethod.GET)
 	public String questionInsert() {
 		System.out.println("questioInsertForm");
-		return "question.questionInsert";
+		return "question/questionInsert";
 		
 	}
 
