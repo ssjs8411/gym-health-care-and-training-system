2d3df452271a5dcf7aed231356eaaa664a8985fd
Administrator <Administrator@smart20124>
2018-10-08 11:47:48 +0900

    [공세준] 권한별 로그인 및 세션처리 수정 및 주석추가
    1. Login.java
    	- 로그인시 세션처리 후 정보 저장 VO(DTO) 수정
    
    2. LoginService.java
    	- 주석 추가
    	- 권한 별 로그인 메서드 호출후 리턴타입 Login 클래스타입으로 수정
    	- 트레이너 로그인 및 세션처리 메서드 추가
    
    3. LoginDao.java
    	- 주석 추가
    	- 권한 별 로그인 메서드 호출후 리턴타입 Login 클래스타입으로 수정
    	- 트레이너 로그인 및 세션처리 메서드 추가
    
    4. LoginMapper.xml
    	- Join 쿼리 수정(헬스장명 가져오게)
    	- 쿼리 실행후 결과 타입 Login 클래스 타입으로 변경
    
    5. 트레이너 패키지 및 클래스, XML 추가
    	- Trainer.java(VO)
    	- TrainerDao.java
    	- TrainerService.java
    	- TrainerMapper.xml

--- GymHealthCareAndTrainingSystem/src/main/java/com/cafe24/chgs8411/login/controller/LoginController.java
+++ GymHealthCareAndTrainingSystem/src/main/java/com/cafe24/chgs8411/login/controller/LoginController.java
@@ -1,3 +1,6 @@
+// 2018. 10. 08. 공세준
+// 로그인 컨트롤러
+
 package com.cafe24.chgs8411.login.controller;
 
 import java.util.Map;
@@ -11,29 +14,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.cafe24.chgs8411.login.service.Login;
 import com.cafe24.chgs8411.login.service.LoginService;
+import com.cafe24.chgs8411.trainer.service.Trainer;
 import com.cafe24.chgs8411.healthclubsAdmin.service.HealthclubsAdmin;
 import com.cafe24.chgs8411.join.service.Member;
 
+// 로그인 컨트롤러 로 로그인 관련 요청을 처리합니다.
 @Controller
 public class LoginController {
 	
+	// loginService 빈을 Autowired 하여 사용합니다.
 	@Autowired
 	private LoginService loginService;
 	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 	
+	// /login POST 방식 요청을 처리하는 메서드 입니다.
+	// 요청을 처리후 /WEB-INF/views/home.jsp로 이동합니다.
 	@RequestMapping(value="/login",method=RequestMethod.POST)
 	public String login(Member member,HttpSession session) {
 		
 		Map<String,Object> result=loginService.login(member);
 		int i=(Integer)(result.get("result"));
 		
+		// loginService 빈 객체에서 login 메서드 호출후 결과값이 1인 경우 받아온 회원 정보를 세션처리 합니다.
 		if(i==1) {
 			session.setAttribute("result", result.get("result"));
 			session.setAttribute("memberSessionId", member.getMember_id());
 			session.setAttribute("memberSessionName", result.get("memberSessionName"));
 			session.setAttribute("memberSessionNo", result.get("memberSessionNo"));
+			session.setAttribute("healthclubsName", result.get("healthclubsName"));
 		}else {
 			logger.debug("login fail");
 			return "main";
@@ -41,36 +50,45 @@ public class LoginController {
 		return "home";
 	}
 	
-	
+	// /logout GET 방식 요청을 처리하는 메서드 입니다.
+	// 요청을 처리후 /WEB-INF/views/main.jsp로 redirect 합니다
 	@RequestMapping(value = "/logout", method = RequestMethod.GET)
 	public String logout(HttpSession session) {
 		session.invalidate();
 		return "redirect:/";
 	}
 	
+	// /adminLogin GET 방식 요청을 처리하는 메서드 입니다.
+	// 요청을 처리후 /WEB-INF/views/login/adminLogin.jsp로 이동 합니다
 	@RequestMapping(value="/adminLogin",method=RequestMethod.GET)
 	public String adminLogin() {
 		
 		return "login/adminLogin";
 	}
 	
+	// /trainerLogin GET 방식 요청을 처리하는 메서등 입니다.
+	// 요청을 처리후  /WEB-INF/views/login/trainerLogin.jsp로 이동 합니다
 	@RequestMapping(value="/trainerLogin",method=RequestMethod.GET)
 	public String trainerLogin() {
 		
 		return "login/trainerLogin";
 	}
 	
+	// /adminLogin POST 방식 요청을 처리하는 메서드 입니다.
+	// 요청을 처리후 /WEB-INF/views/home.jsp로 이동합니다.
 	@RequestMapping(value="/adminLogin",method=RequestMethod.POST)
 	public String adminLogin(HealthclubsAdmin healthclubsAdmin, HttpSession session) {
 		
 		Map<String,Object> result=loginService.adminLogin(healthclubsAdmin);
 		int i=(Integer)(result.get("result"));
 		
+		// loginService 빈 객체에서 adminLogin 메서드 호출후 결과값이 1인 경우 받아온 관리자 정보를 세션처리 합니다.
 		if(i==1) {
 			session.setAttribute("result", result.get("result"));
 			session.setAttribute("adminSessionId", healthclubsAdmin.getHealthclubs_admin_id());
 			session.setAttribute("adminSessionName", result.get("adminSessionName"));
 			session.setAttribute("adminSessionNo", result.get("adminSessionNo"));
+			session.setAttribute("healthclubsName", result.get("healthclubsName"));
 		}else {
 			logger.debug("login fail");
 			return "main";
@@ -78,9 +96,26 @@ public class LoginController {
 		return "home";
 	}
 	
+	// /trainerLogin POST 방식 요청을 처리하는 메서드 입니다.
+	// 요청을 처리후 /WEB-INF/views/home.jsp로 이동합니다.
 	@RequestMapping(value="/trainerLogin",method=RequestMethod.POST)
-	public String trainerLogin(Login login, HttpSession session) {
-		System.out.println("trainer");
+	public String trainerLogin(Trainer trainer, HttpSession session) {
+		
+		Map<String,Object> result=loginService.trainerLogin(trainer);
+		int i=(Integer)(result.get("result"));
+		
+		
+		// loginService 빈 객체에서 trainerLogin 메서드 호출후 결과값이 1인 경우 받아온 트레이너 정보를 세션처리 합니다.
+		if(i==1) {
+			session.setAttribute("result", result.get("result"));
+			session.setAttribute("trainerSessionId", trainer.getTrainer_id());
+			session.setAttribute("trainerSessionName", result.get("trainerSessionName"));
+			session.setAttribute("trainerSessionNo", result.get("trainerSessionNo"));
+			session.setAttribute("healthclubsName", result.get("healthclubsName"));
+		}else {
+			logger.debug("login fail");
+			return "main";
+		}
 		return "home";
 	}
 
--- GymHealthCareAndTrainingSystem/src/main/java/com/cafe24/chgs8411/login/service/LoginDao.java
+++ GymHealthCareAndTrainingSystem/src/main/java/com/cafe24/chgs8411/login/service/LoginDao.java
@@ -1,3 +1,6 @@
+// 2018. 10. 08. 공세준
+// 로그인 DAO
+
 package com.cafe24.chgs8411.login.service;
 
 import org.mybatis.spring.SqlSessionTemplate;
@@ -7,32 +10,67 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.cafe24.chgs8411.login.service.LoginDao;
+import com.cafe24.chgs8411.trainer.service.Trainer;
 import com.cafe24.chgs8411.healthclubsAdmin.service.HealthclubsAdmin;
 import com.cafe24.chgs8411.join.service.Member;
 
+//Repository 는 데이터 접근 계층 즉 DAO 라는 것을 알려주는 어노테이션
 @Repository
 public class LoginDao {
+	
+	// Autowired 는 스프링이 빈의 요구 사항과 매칭 되는 애플리케이션 컨텍스트상에서 다른 빈을 찾아 빈 간의 의존성을 자동으로 만족시키도록 하는 수단입니다.
+	// SqlSessionTemplate 클래스타입으로 sqlSessionTemplate 변수를 선언합니다.
+    // 쿼리를 불러오기 위해 사용합니다. 
 	@Autowired SqlSessionTemplate sqlSession;
 	
 	private static final Logger logger = LoggerFactory.getLogger(LoginDao.class);
+	
+	// 쿼리를 가져오기 위해 경로를 저장 하는 참조타입 변수를 선언하고 final 로 고정하고 private 로 접근을 허용하지 않습니다.
 	final String NS = "com.cafe24.chgs8411.login.service.LoginMapper.";
 	
 	
+    // 설명 : login 메서드명으로 호출하면 member 객체를 받아 DB에 접근하여 데이터 존재 여부를 search 합니다.
+    // 매개변수 : 매개변수는 Member 클래스 타입으로 회원 정보를 받습니다.
+    // 리턴타입 : 리턴 타입은 int 기본 타입으로 쿼리의 실행 여부를 1과 0으로 리턴합니다.
 	public int login(Member member) {
 		return sqlSession.selectOne(NS+"login",member);
 	}
 	
-	public Member loginSession(String memberId){
+	// 설명 : loginSession 메서드명으로 호출하면 String memberId 를 받아서 DB에 접근하여 데이터 존재 여부를 확인하고 세션정보를 받아옵니다.
+    // 매개변수 : 매개변수는 String 참조타입으로 memberId륿 받습니다.
+	// 리턴타입 : 리턴 타입은 Login 클래스 타입으로 회원 세션정보를 리턴합니다.
+	public Login loginSession(String memberId){
 		return sqlSession.selectOne(NS+"loginSession",memberId);
 	}
 	
+	// 설명 : adminLogin 메서드명으로 호출하면 HealthclubsAdmin 클래스 타입으로 관리자 로그인 정보를 받아서 DB에 접근하여 데이터 존재 여부를 search 합니다.
+	// 매개변수 : 매개변수는 HealthclubsAdmin 클래스 타입으로 관리자 로그인 정보를 받습니다. 
+	// 리턴타입 : 리턴 타입은 int타입으로 쿼리의 실행 여부를 1과 0으로 리턴합니다.
 	public int adminLogin(HealthclubsAdmin healthclubsAdmin) {
 		return sqlSession.selectOne(NS+"adminLogin",healthclubsAdmin);
 		
 	}
 	
-	public HealthclubsAdmin adminLoginSession(String adminId){
-		return sqlSession.selectOne(NS+"adminLoginSession",adminId);
+	// 설명 : adminLoginSession 메서드명으로 String adminId를 받아서 DB에 접근하여 데이터 존재 여부를 확인하고 세션정보를 받아옵니다.
+	// 매개변수 : 매개변수는 String 참조 타입으로 adminId 를 받습니다.
+	// 리턴타입 : 리턴 타입은 Login 클래스 타입으로 관리자의 세션정보를 리턴합니다.
+	public Login adminLoginSession(String adminId){
+		return sqlSession.selectOne(NS+"adminLoginSession", adminId);
+	}
+	
+	// 설명 : trainerLogin 메서드명으로 호출하면 HealthclubsAdmin 클래스 타입으로 관리자 로그인 정보를 받아서 DB에 접근하여 데이터 존재 여부를 search 합니다.
+	// 매개변수 : 매개변수는 Trainer 클래스 타입으로 관리자 로그인 정보를 받습니다. 
+	// 리턴타입 : 리턴 타입은 int타입으로 쿼리의 실행 여부를 1과 0으로 리턴합니다.
+	public int trainerLogin(Trainer trainer) {
+		return sqlSession.selectOne(NS+"trainerLogin",trainer);
+		
+	}
+	
+	// 설명 : trainerLoginSession 메서드명으로 String trainerId를 받아서 DB에 접근하여 데이터 존재 여부를 확인하고 세션정보를 받아옵니다.
+	// 매개변수 : 매개변수는 String 참조 타입으로 trainerId 를 받습니다.
+	// 리턴타입 : 리턴 타입은 Login 클래스 타입으로 관리자의 세션정보를 리턴합니다.
+	public Login trainerLoginSession(String trainerId){
+		return sqlSession.selectOne(NS+"trainerLoginSession",trainerId);
 	}
 	
 }
