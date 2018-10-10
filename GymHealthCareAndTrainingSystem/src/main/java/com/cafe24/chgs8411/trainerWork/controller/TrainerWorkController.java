d773d85016bdfe1a34ff7ac7e9c7d374255d55fa
Administrator <Administrator@smart20116>
2018-10-10 17:12:24 +0900

    [정민수] 트레이너 근태 관리 (출퇴근패키지 및 클래스생성) 1.TrainerWorkController.java 출퇴근 컨트롤러 생성 2.TrainerWork.java 출퇴근 dto 클래스 생성 3.TrainerWorkDao.java 출퇴근 Dao 클래스 생성 4.TrainerWorkService.java 출퇴근 서비스 클래스 생성 5.TrainerWorkMapper.xml 출퇴근 쿼리등록할 Mapper생성

---
+++ GymHealthCareAndTrainingSystem/src/main/java/com/cafe24/chgs8411/trainerWork/controller/TrainerWorkController.java
@@ -0,0 +1,5 @@
+package com.cafe24.chgs8411.trainerWork.controller;
+
+public class TrainerWorkController {
+
+}
---
+++ GymHealthCareAndTrainingSystem/src/main/java/com/cafe24/chgs8411/trainerWork/service/TrainerWork.java
@@ -0,0 +1,5 @@
+package com.cafe24.chgs8411.trainerWork.service;
+
+public class TrainerWork {
+
+}
