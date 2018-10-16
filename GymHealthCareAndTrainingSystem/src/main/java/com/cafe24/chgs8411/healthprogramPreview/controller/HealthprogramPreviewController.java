8c2a52831581c5c2279ede62d5d575da07fa573b
Administrator <Administrator@smart20116>
2018-10-16 17:27:02 +0900

    [김소희] healthprogramPreview 추가
    1. com.cafe24.chgs8411.healthprogramPreview.controller 패키지 생성
      -HealthprogramPreviewController.java 생성
    
    2. com.cafe24.chgs8411.healthprogramPreview.service 패키지 생성
      -HealthprogramPreview.java 생성
      -HealthprogramPreviewDao.java 생성
      -HealthprogramPreviewServie.java 생성
      -HealthprogramPreviewMapper.xml 생성

---
+++ GymHealthCareAndTrainingSystem/src/main/java/com/cafe24/chgs8411/healthprogramPreview/controller/HealthprogramPreviewController.java
@@ -0,0 +1,6 @@
+// 2018-10-16 김소희
+package com.cafe24.chgs8411.healthprogramPreview.controller;
+
+public class HealthprogramPreviewController {
+
+}
---
+++ GymHealthCareAndTrainingSystem/src/main/java/com/cafe24/chgs8411/healthprogramPreview/service/HealthprogramPreview.java
@@ -0,0 +1,53 @@
+// 2018-10-16 김소희
+package com.cafe24.chgs8411.healthprogramPreview.service;
+
+public class HealthprogramPreview {
+
+		private int healthprogram_preview_no;
+		private int member_no;
+		private int health_program_no;
+		private String preview_title;
+		private String preview_content;
+		private String preview_date;
+		
+		public int getHealthprogram_preview_no() {
+			return healthprogram_preview_no;
+		}
+		public void setHealthprogram_preview_no(int healthprogram_preview_no) {
+			this.healthprogram_preview_no = healthprogram_preview_no;
+		}
+		public int getMember_no() {
+			return member_no;
+		}
+		public void setMember_no(int member_no) {
+			this.member_no = member_no;
+		}
+		public int getHealth_program_no() {
+			return health_program_no;
+		}
+		public void setHealth_program_no(int health_program_no) {
+			this.health_program_no = health_program_no;
+		}
+		public String getPreview_title() {
+			return preview_title;
+		}
+		public void setPreview_title(String preview_title) {
+			this.preview_title = preview_title;
+		}
+		public String getPreview_content() {
+			return preview_content;
+		}
+		public void setPreview_content(String preview_content) {
+			this.preview_content = preview_content;
+		}
+		public String getPreview_date() {
+			return preview_date;
+		}
+		public void setPreview_date(String preview_date) {
+			this.preview_date = preview_date;
+		}
+		
+		
+
+
+}
